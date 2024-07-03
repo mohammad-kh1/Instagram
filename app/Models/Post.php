394236d46a75be65671797d60053d1a427338d5a
mohammad <mohammadkh12221@gmail.com>
2024-07-03 18:49:44 +0330

    post model

---
+++ app/Models/Post.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    use HasFactory;
+
+    protected  $guarded = [];
+
+    protected $casts = [
+        "hide_like" => "boolean",
+        "allow_commenting" => "boolean",
+
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+//    public function media()
+//    {
+//        return $this->morphMany(Media::class , "mediable");
+//    }
+
+
+
+}
---
+++ database/factories/PostFactory.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Post;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
+ */
+class PostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            "user_id" => User::inRandomOrder()->first()->id,
+            "description" => $this->faker->text(255),
+            "location" => $this->faker->city,
+            "hide_like_view" => $this->faker->boolean(),
+            "allow_commenting" => $this->faker->boolean(),
+            "type" => $this->faker->randomElement(["post", "reel"]),
+        ];
+    }
+
+    public function configure()
+    {
+        return $this->afterCreating(function(Post  $post){
+            if($post->type == "media"){
+             Media::facotry()->reel->create(["mediable_type" => get_class($post) , "mediable_id" => $post->id]);
+            }else{
+                Media::facotry()->post->create(["mediable_type" => get_class($post) , "mediable_id" => $post->id]);
+
+            }
+        });
+    }
+}
