29324eafeeab768ac338dd8dce41469edd1edf64
mohammad <mohammadkh12221@gmail.com>
2024-07-03 16:31:34 +0330

    sidebar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -31,11 +31,9 @@
         </div>
         <div class="drawer-side">
             <label for="my-drawer-2" aria-label="close sidebar" class="drawer-overlay"></label>
-            <ul class="menu bg-base-200 text-base-content min-h-full w-80 p-4">
-                <!-- Sidebar content here -->
-                <li><a>Sidebar Item 1</a></li>
-                <li><a>Sidebar Item 2</a></li>
-            </ul>
+
+            @include("layouts.sidebar")
+
         </div>
     </div>
     </body>
