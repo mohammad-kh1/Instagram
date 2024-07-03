4b133f344d0911d8fb7cf493114b5b7ee0a71fc8
mohammad <mohammadkh12221@gmail.com>
2024-07-03 17:35:49 +0330

    mobile header

--- resources/views/livewire/home.blade.php
+++ resources/views/livewire/home.blade.php
@@ -1,11 +1,29 @@
-<div class="w-full">
+<div class="w-full h-ful">
 
     {{--    Header   --}}
-    <header></header>
+    <header class="md:hidden sticky top-0 bg-white">
+        <div class="grid grid-cols-12 gap-2 items-center">
+            <div class="col-span-3">
+                <img src="{{ asset('assets/logo.png')  }}" class="h-12 max-w-lg w-full" alt="logo">
+            </div>
+            <div class="col-span-8 flex justify-center px-2">
+                <input type="text" placeholder="Search" class="border-0 outline-none w-full focus:outline-none bg-gray-100 rounded-lg focus:ring-0 hover:ring-0">
+            </div>
+            <div class="col-span-1 flex justify-center">
+                <a href="#">
+                    <span>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2.4" stroke="currentColor" class="size-6">
+                          <path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
+                        </svg>
+                    </span>
+                </a>
+            </div>
+        </div>
+    </header>
 
     {{--  Main  --}}
     <main class="grid lg:grid-cols-12 gap-8 md:mt-10">
-        <aside class="lg:col-span-8 border overflow-hidden h-[1000px]">
+        <aside class="lg:col-span-8 overflow-hidden">
             {{--     Stories       --}}
             <section class="">
                 <ul class="flex overflow-x-auto items-center gap-2 scrollbar-hide">
@@ -20,7 +38,7 @@
 
         </aside>
 
-        <aside class="lg:col-span-4 border hidden lg:block p-4">
+        <aside class="lg:col-span-4 hidden lg:block p-4">
 
 
                 <div class="flex items-center gap-2 p-2">
