65250c7cc76ccc5033ad229b1fbb34ca4850a990
mohammad <mohammadkh12221@gmail.com>
2024-07-04 16:25:06 +0330

    comment factory and seeder'

--- app/Livewire/Home.php
+++ app/Livewire/Home.php
@@ -19,7 +19,7 @@ class Home extends Component
 
     function __construct()
     {
-        $this->posts = Post::latest()->get();
+        $this->posts = Post::latest()->with("comments")->get();
     }
 
     #[On("post-created")]
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Comment;
 use App\Models\Post;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
@@ -21,8 +22,13 @@ class DatabaseSeeder extends Seeder
 //            'email' => 'test@example.com',
 //        ]);
 
-        Post::factory(15)->create(["type"=>"reel"]);
-        Post::factory(15)->create(["type"=>"post"]);
+        Post::factory(15)->hasComments(10)->create(["type"=>"reel"]);
+        Post::factory(15)->hasComments(10)->create(["type"=>"post"]);
 
+        //
+        Comment::limit(50)->each(function($comment){
+            $comment::factory(rand(1,5))->isReply($comment->commentable)->create(["parent_id" => $comment->id]);
+        });
     }
+
 }
