4b07c6d3c0e98769a71fc2123c9f5eebdd9a64ec
mohammad <mohammadkh12221@gmail.com>
2024-07-05 19:10:29 +0330

    [200~Add instant comments to Post Item ~

--- app/Livewire/Post/Item.php
+++ app/Livewire/Post/Item.php
@@ -2,12 +2,30 @@
 
 namespace App\Livewire\Post;
 
+use App\Models\Comment;
+use App\Models\Post;
 use Livewire\Component;
 
 class Item extends Component
 {
 
     public $post;
+
+    public $body;
+
+    function addComment()
+    {
+        $this->validate(["body"=>"required"]);
+        Comment::create([
+            "body"=>$this->body,
+            "commentable_id" => $this->post->id,
+            "commentable_type" => Post::class,
+            "user_id" => auth()->id(),
+
+        ]);
+
+        $this->reset("body");
+    }
     public function render()
     {
         return view('livewire.post.item');
