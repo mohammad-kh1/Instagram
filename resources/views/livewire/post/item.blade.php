b57ef04b396bfc27846f2d7b722af68c80c12efc
mohammad <mohammadkh12221@gmail.com>
2024-07-05 19:30:22 +0330

    disable commenting

--- resources/views/livewire/post/item.blade.php
+++ resources/views/livewire/post/item.blade.php
@@ -90,19 +90,23 @@
 <footer>
 
     <div class="flex gap-4 items-center my-2">
+{{--        heart--}}
         <span>
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
               <path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
             </svg>
 
         </span>
+        @if($post->allow_commenting)
 
-        <span>
+
+{{--        comments--}}
+        <button onclick="Livewire.dispatch('openModal',{component:'post.view.modal',arguments:{'post':{{$post->id}}}})">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
               <path stroke-linecap="round" stroke-linejoin="round" d="M12 20.25c4.97 0 9-3.694 9-8.25s-4.03-8.25-9-8.25S3 7.444 3 12c0 2.104.859 4.023 2.273 5.48.432.447.74 1.04.586 1.641a4.483 4.483 0 0 1-.923 1.785A5.969 5.969 0 0 0 6 21c1.282 0 2.47-.402 3.445-1.087.81.22 1.668.337 2.555.337Z" />
             </svg>
-
-        </span>
+@endif
+        </button>
 
         <span>
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"  class="bi bi-send w-5 h-5" viewBox="0 0 16 16">
@@ -127,10 +131,16 @@
             {{ $post->description  }}
         </p>
     </div>
+
+
+
+    @if($post->allow_commenting)
+
 {{--  view post modal  --}}
     <button onclick="Livewire.dispatch('openModal',{component:'post.view.modal',arguments:{'post':{{$post->id}}}})" class="text-slate-500/90 text-sm font-medium">View all {{  $post->comments->count() }} comments</button>
 
     @auth()
+{{--show commnets for auth--}}
 
         <ul class="my-2">
             @foreach($post->comments()->where("user_id" , auth()->id())->get() as $comment)
@@ -179,6 +189,8 @@
 
 
     </form>
+    @endif
+
 </footer>
 
 
--- resources/views/livewire/post/view/item.blade.php
+++ resources/views/livewire/post/view/item.blade.php
@@ -71,20 +71,22 @@
         <footer class="mt-auto sticky border-t bottom-0 z-10 bg-white">
 
             <div class="flex gap-4 items-center my-2">
+{{--                heat --}}
         <span>
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
               <path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
             </svg>
-
         </span>
+{{--comment--}}
+                @if($post->allow_commenting)
 
                 <span>
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
               <path stroke-linecap="round" stroke-linejoin="round" d="M12 20.25c4.97 0 9-3.694 9-8.25s-4.03-8.25-9-8.25S3 7.444 3 12c0 2.104.859 4.023 2.273 5.48.432.447.74 1.04.586 1.641a4.483 4.483 0 0 1-.923 1.785A5.969 5.969 0 0 0 6 21c1.282 0 2.47-.402 3.445-1.087.81.22 1.668.337 2.555.337Z" />
             </svg>
-
         </span>
-
+            @endif
+{{--forward--}}
                 <span>
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"  class="bi bi-send w-5 h-5" viewBox="0 0 16 16">
               <path d="M15.854.146a.5.5 0 0 1 .11.54l-5.819 14.547a.75.75 0 0 1-1.329.124l-3.178-4.995L.643 7.184a.75.75 0 0 1 .124-1.33L15.314.037a.5.5 0 0 1 .54.11ZM6.636 10.07l2.761 4.338L14.13 2.576zm6.787-8.201L1.591 6.602l4.339 2.76z"/>
@@ -101,15 +103,19 @@
 
             {{--  likes and views    --}}
             <p class="font-bold text-sm text-black">125,521 likes</p>
-            {{-- naem and commnets --}}
+            {{-- name and commnets --}}
             <div class="flex text-sm gap-2 font-medium text-black">
                 <p>
                     <strong class="font-bold ">{{ $post->user->name }}</strong>
                     {{ $post->description  }}
                 </p>
             </div>
+
+            @if($post->allow_commenting)
+
+
             {{--  view post modal  --}}
-            <button class="text-slate-500/90 text-sm font-medium">View all {{   $post->comments->count()  }} comments</button>
+            <button class="text-slate-500/90 text-sm font-medium">Total {{   $post->comments->count()  }} comments</button>
             {{--  leave comment  --}}
             <form
                 wire:key="{{time()}}"
@@ -137,6 +143,8 @@
 
 
             </form>
+            @endif
+
         </footer>
 
 
