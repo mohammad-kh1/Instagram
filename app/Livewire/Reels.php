be25335af774c2b008479a4bd8958956644aa3ad
mohammad <mohammadkh12221@gmail.com>
2024-07-06 13:37:53 +0330

    Show Posts Reels

--- app/Livewire/Reels.php
+++ app/Livewire/Reels.php
@@ -2,12 +2,33 @@
 
 namespace App\Livewire;
 
+use App\Models\Post;
+use Livewire\Attributes\On;
 use Livewire\Component;
 
 class Reels extends Component
 {
+    #[On("closeModal")]
+    public function reverUrl()
+    {
+        $this->js("history.replaceState({},'','/')");
+    }
+
+    function togglePostLike(Post $post)
+    {
+        abort_unless(auth()->check() , 401);
+        auth()->user()->toggleLike($post);
+    }
+
+    function toggleFavorite(Post $post)
+    {
+        abort_unless(auth()->check() , 401);
+        auth()->user()->toggleFavorite($post);
+    }
+
     public function render()
     {
-        return view('livewire.reels');
+        $posts = Post::limit(20)->where("type" , "reel")->get();
+        return view('livewire.reels')->with(["posts" => $posts]);
     }
 }
